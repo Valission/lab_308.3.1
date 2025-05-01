c7915066bb36d4a4cde4ac7803a3597d94df9ef5
Valission <FernandomvHull@gmail.com>
2025-05-01 14:10:55 -0400

    Expanding_Functionality

---
+++ loop.js
@@ -0,0 +1,28 @@
+for(let i = 10; i > 0 ; i--){
+    console.log (i)
+}
+console.log("end")
+
+for(let i = 0; i <= 10 ; i++){
+    if (i % 2 == 0)
+        console.log(i)
+}
+console.log("end")
+
+for(let i = 0; i <= 10 ; i++){
+    if (i % 2 != 0)
+        console.log(i)
+}
+console.log("end")
+
+for(let i = 6; i <= 60 ; i++){
+    if (i % 3 == 0)
+        console.log(i)
+}
+console.log("end")
+let num_sign = ""
+for(let i = 0; i < 7 ; i++){
+    num_sign += "#"
+        console.log(num_sign)
+}
+console.log("end")
